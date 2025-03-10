3697455e1ed7b93f67d65f49dc0a0b0fa8621b46
KenoGPT <tsukudumi@yahoo.co.jp>
2025-03-11 06:38:53 +0900

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -48,9 +48,7 @@ if CACHE_FILE:
 
 def extract_titles(html: str):
     pattern = (
-        r'<h3 class="title01">
-\s*<a href="([^"]+)">([^<]+)</a>
-\s*</h3>'
+        r'<h3 class="title01">\s*<a href="([^"]+)">([^<]+)</a>\s*</h3>'
     )
     return re.findall(pattern, html)
 
